1b54fb69bc21aa00ad569944414b158af7a3b5c0
Flor Tello <flor.26071994@gmail.com>
2017-07-06 09:33:26 -0500

    Agregando modal

--- src/assets/js/componentes/modal.js
+++ src/assets/js/componentes/modal.js
@@ -1,7 +1,11 @@
 'use strict';
 
 const Modal = (modal,index,update) => {
-  console.log(index);
+  const src=state.data.boards[index].image.original.url;
+  var titulo = state.data.boards[index].metadata.note;
+  if(state.data.boards[index].metadata.article){
+    titulo = "";
+  }
   // const modal = $('<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true"></div>');
   const header_modal = $('<button type="button" class="close modal--close" aria-label="Close"></button>');
   const close = $('<span aria-hidden="true" data-dismiss="modal">&times;</span>');
@@ -11,7 +15,7 @@ const Modal = (modal,index,update) => {
   const title = $('<h2 class="modal-title" id="exampleModalLabel">Modal title</h2>');
   const save = $('<button type="button" class="btn btn-danger pull-right" data-dismiss="modal">Save</button>');
   const modal_body = $('<div class="modal-body"></div>');
-  const img = $('<img src="https://s-media-cache-ak0.pinimg.com/originals/0f/11/a2/0f11a2a6a20a3307df7d411cd51fdc57.jpg" class="modal-content--img" alt="" width="100%">');
+  const img = $('<img src="'+src+'" class="modal-content--img" alt="" width="100%">');
   const footer = $('<div class="modal-footer"></div>');
   const btn_footer = $('<button type="button" class="btn btn-info">Read it</button>');
   header_modal.append(close);
